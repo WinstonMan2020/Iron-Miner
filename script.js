d445d4a4def6667ab07bad3484a716d5407af994
WinstonMan2020 <pytthon.joshua@gmail.com>
2021-11-14 18:34:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-function clickCounter() {
-    document.getElementById("result").innerHTML = "You have clicked the button " + localStorage.clickcount + " time(s).";
+function bakeCookie() {
+    document.getElementById("cookies").innerHTML = "Cookies: " + localStorage.cookies;
     if (typeof(Storage) !== "undefined") {
-      if (localStorage.clickcount) {
-        localStorage.clickcount = Number(localStorage.clickcount)+1;
-      } else {
-        localStorage.clickcount = 1;
-      }
-    } else {
-      document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
+        if (localStorage.cookies) {
+            localStorage.cookies = Number(localstorage.cookies) + 1;
+        } else {
+            localstorage.cookies = 1;
+        }
     }
-  }
+}
+
